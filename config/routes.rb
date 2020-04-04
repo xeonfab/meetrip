98056a2b5cc8b6fda9ce6105f729dbcfc78cba26
Fabien Riou <fabien.riou92@gmail.com>
2020-04-04 13:23:59 +1300

    share

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get 'fr', to: 'pages#home_fr'
 
 end
